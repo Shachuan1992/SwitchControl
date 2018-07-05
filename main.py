19f69ceac99c824cefd7535c788e83c3548a6439
Shachuan1992 <shac@chinaunicom.cn>
2018-07-05 17:51:40 +0800

    在main.py中增加了注释。现在main.py可以自动执行命令，但是还无法判断反馈的字符串。

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # -*- coding:utf-8 -*-
-
+"""
+这是自动查光衰的程序，可以一键登录olt。
+内部集成了常用命令集合，可以自动反馈光衰
+"""
 __author__ = 'shachuan'
 
 
@@ -10,11 +13,10 @@ from commands import *
 host = '127.0.0.1'
 port = 5000
 
-
 tn = telnetlib.Telnet(host,port=port)
 write_command(tn,'conf')
-time.sleep(1)
+time.sleep(2)
 write_command(tn,'terminal')
-time.sleep(1)
+time.sleep(2)
 write_command(tn,'exit')
 result = tn.read_very_eager().decode()

8c9ee5322f84976485a2f3fef96ef17da2b4cc93
Shachuan1992 <shac@chinaunicom.cn>
2018-07-09 17:47:39 +0800

    大修改，修改了get_info函数中的正则表达式的规则，用re.search替换了re.findall，这样就不用eval函数去掉两边的特殊字符了。并修复了一个小bug，ONUID的正则表达式中，以前的d{1,2}改成了d{1,4}，这样列数多了以后不会再崩溃

--- telnet.py
+++ telnet.py
@@ -34,37 +34,35 @@ def write_command(instance,command):
 
 
 
-#从光衰表中获取OLTP的IP，设备型号Device_ID，Pon口PON_PORT以及ONU_ID
+#从光衰表中获取OLTP的IP，设备型号Device_ID，IP,PON_PORT以及ONU_ID
 def get_info(file,account):
-    global IP,DEVICE_ID,ONU_ID,SLOT,BOARD,PORT#声明全局变量
+    global IP,DEVICE_ID,ONU_ID,PON,SLOT,BOARD,PORT#声明全局变量
     df = pd.read_excel(file,'光衰清单')#打开光衰表文件
-    IP = str(re.search(r'(\d+.\d+.\d+.\d+)', str(df[df['账号'] == account]['OLT-IP地址']), re.M).group(1))  # 使用正则表达式匹配IP
-    PON = str(re.search(r'(\d/\d/\d)', str(df[df['账号'] == account]['PON口']), re.M).group(1))  # 使用正则表达式匹配Pon口
-    DEVICE_ID = str(re.search(r'([A-Z][A-Z0-9]{3,7})', str(df[df['账号'] == account]['设备型号']), re.M).group(1))  # 使用正则表达式匹配设备型号
-    ONU_ID = str(re.search(r'^\d{1,4}\s+(\d{1,2})$', str(df[df['账号'] == account]['ONUID']), re.M).group(1))  # 使用正则表达式匹配ONUID
-    EPON = PON.split('/')
-    SLOT = EPON[0]
-    BOARD = EPON[1]
-    PORT = EPON[2]
+    IP = str(re.search(r'(\d+.\d+.\d+.\d+)', str(df[df['账号'] == account]['OLT-IP地址']), re.M).group(1))              #使用正则表达式匹配IP
+    PON = str(re.search(r'(\d/\d/\d)', str(df[df['账号'] == account]['PON口']), re.M).group(1))                         # 使用正则表达式匹配Pon口
+    DEVICE_ID = str(re.search(r'([A-Z][A-Z0-9]{3,7})', str(df[df['账号'] == account]['设备型号']), re.M).group(1))       # 使用正则表达式匹配设备型号
+    ONU_ID = str(re.search(r'^\d{1,4}\s+(\d{1,2})$', str(df[df['账号'] == account]['ONUID']), re.M).group(1))           # 使用正则表达式匹配ONUID
+    PON_PORT = PON.split('/')
+    SLOT = PON_PORT[0]
+    BOARD = PON_PORT[1]
+    PORT = PON_PORT[2]
 
 
 
 def command_make(Device):
     global commands
     commands = []
-    if Device == 'MA5683T':
-        command_interface = 'interface epon ' + SLOT + '/' + BOARD
-        command_opticai_info = 'display ont optical-info ' + PORT + ' ' + ONU_ID
-    elif Device == 'MA5680T':
-        command_interface = 'interface epon ' + SLOT + '/' + BOARD
-        command_opticai_info = 'display ont optical-info ' + PORT + ' ' + ONU_ID
+    if (Device == 'MA5683T') or (Device == 'MA5680T'):
+        commands.append('lyread')
+        commands.append('read123')
+        commands.append('enable')
+        commands.append('config')
+        commands.append('interface epon ' + SLOT + '/' + BOARD)
+        commands.append('display ont optical-info ' + PORT + ' ' + ONU_ID)
+    elif Device == 'C300':
+        commands.append('show pon power onu-rx epon-onu_'+PON+':'+ONU_ID)
     else:
-        command_interface = 'conf'
-        command_opticai_info = 'terminal'
-        quit = 'exit'
-    commands.append(command_interface)
-    commands.append(command_opticai_info)
-
+        print("错误！！！没有这种设备，需添加命令")
 
 
 
@@ -72,17 +70,21 @@ def telnet():
 
     tn = telnetlib.Telnet(host=IP, port=5000)   # 开启Telnet
 
-    command_make(Device=DEVICE_ID)              # 根据设备型号，制作命令
-
+    command_make(Device=DEVICE_ID)            # 根据设备型号，制作命令
     # 输入命令并执行
     for command in commands:
         write_command(tn,command)
         time.sleep(0.5)
-        # reply = (str(tn.read_very_eager(),encoding='utf-8'))
-        # sample = reply.replace('(', '').replace(')', '')
-        # RxOptical = re.search(r"^\s+Rx optical powerdBm\s+:(.+)$", sample, re.M)
-        # RxPower = RxOptical.group(1).lstrip(' ')
-
+    reply = (str(tn.read_very_eager(),encoding='utf-8'))
+    if (DEVICE_ID == 'MA5683T') or (DEVICE_ID == 'MA5683T'):
+        sample = reply.replace('(', '').replace(')', '')
+        RxOptical = re.search(r"^\s+Rx optical powerdBm\s+:(.+)$", sample, re.M)
+        RxPower = RxOptical.group(1).lstrip(' ')
+        print("光衰值："+RxPower+'\n')
+    else:
+        RxOptical = re.search(r"^epon-onu_\d/\d/\d:\d\s+(.+)$",reply,re.M)
+        RxPower = RxOptical.group(1).rstrip('(dbm)')
+        print("光衰值：" + RxPower+'\n')
 
     #在线命令调试
     # x1 = True
@@ -91,6 +93,3 @@ def telnet():
     #     write_command(tn,x1)
     #     time.sleep(0.5)
     #     print(str(tn.read_very_eager(), encoding='utf-8'))
-
-
-
